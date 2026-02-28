74e64a7f9c597219c4b61382297d457ecb3725ba
Benison Abraham <benisonkt@gmail.com>
2026-02-28 13:28:16 +0000

    Back to original

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,24 @@
-// console.log("FILE EXECUTED")
-
-// import { Hono } from 'hono'
-// import { cors } from 'hono/cors'
-// import {logger} from 'hono/logger'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import {logger} from 'hono/logger'
 // import api from './api'
 
-// const app = new Hono()
+const app = new Hono()
 
-// app.use(cors())
-// app.use(logger())
+app.use(cors())
+app.use(logger())
 
-// app.get('/', (c) => {
-//   return c.json({ status: 'up' })
-// })
+app.get('/', (c) => {
+  return c.json({ status: 'up' })
+})
 
 // app.route('/api', api)
-// console.log("BEFORE SERVER START")
-
-// Bun.serve({
-//   fetch: app.fetch,
-//   port: 3000,
-//   hostname: '0.0.0.0',   // THIS IS THE KEY
-// })
-
-// console.log("AFTER SERVER START")
+console.log("BEFORE SERVER START")
 
 Bun.serve({
+  fetch: app.fetch,
   port: 3000,
-  hostname: "0.0.0.0",
-  fetch() {
-    return new Response("WORKING")
-  }
+  hostname: '0.0.0.0',   // THIS IS THE KEY
 })
+
+console.log("AFTER SERVER START")
